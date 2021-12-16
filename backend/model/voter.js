09906f2fde0fc503ace15240e1e8804c8859f290
Restu Indera <rstx32@gmail.com>
2021-12-16 20:03:45 +0700

    voter schema

---
+++ backend/model/voter.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+// schema voter
+const Voter = mongoose.model('Voter', {
+  username: {
+    type: String,
+    required: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: String,
+  photo: String,
+})
+
+module.exports = {
+  Voter,
+}
