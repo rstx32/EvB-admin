0283ac29c65ad0587cb234cc73f7bc824e59666a
Restu Indera <rstx32@gmail.com>
2021-12-16 20:11:44 +0700

    delete unnecessary method

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,11 @@ const CryptoJS = require('crypto-js')
 const { ObjectId } = require('mongodb')
 mongoose.connect(uri)
 
+// get all voter
+const getVoter = () => {
+  Voter.find()
+}
+
 // add voter
 const addVoter = (newVoter) => {
   new Voter({
@@ -25,87 +30,6 @@ const deleteVoter = async (id) => {
 
 // edit voter
 const editVoter = (id) => {}
-
-// const voter1 = new Voter({
-//   username: 'rstx',
-//   fullname: 'restu indera',
-//   password: '0060ef32e6559dd496b32d950f229695a3994d3c28fa09b0ecfc2c5a48180ae9'
-// })
-// voter1.save().then((voter) => console.log(voter))
-
-// client.connect((error, client) => {
-//   if (error) return console.log(`koneksi gagal`)
-
-//   // get all voters, project username & password only
-//   const getAllVoters = db
-//     .find()
-//     .project({
-//       username: 1,
-//       password: 1,
-//     })
-//     .toArray((error, result) => {
-//       if (error) return console.log(error)
-//       console.log(result)
-//     })
-
-// find a voters
-//   db.findOne(
-//     { username: 'xtsr' },
-//     {
-//       projection: {
-//         username: 1,
-//         password: 1,
-//       },
-//     },
-//     (error, result) => {
-//       console.log(result)
-//     }
-//   )
-
-// insert a voter
-// db.insertOne(
-//   {
-//     username: 'sania',
-//     fullname: 'sania nastasya viora',
-//     password:
-//       '643f67ca0cce632c8ddf6359f4abf2cc51a8f6cbb0197b633ebfc06646ad0241',
-//     voting: false,
-//   }
-// ).then((result) => {
-//     console.log(result)
-// }).catch((error) => {
-//     if(error.code===11000)
-//         console.log(`data sudah ada!`);
-// })
-
-// edit a voters
-//   db.updateOne(
-//     {
-//       username: 'rstx',
-//     },
-//     {
-//       $set: {
-//         fullname: 'restu indera dwihandoko',
-//       },
-//     },
-//     (error, result) => {
-//       if (error) return console.log('gagal mengubah data')
-//       else return console.log('data berhasil diubah')
-//     }
-//   )
-
-// delete a voters
-//   db.deleteOne({
-//     username: 'sania',
-//   })
-//     .then((result) => {
-//       if (result.deletedCount === 0) console.log('data tidak dihapus!')
-//       console.log(`berhasil menghapus data!`)
-//     })
-//     .catch((error) => {
-//       console.log(error)
-//     })
-// })
 
 module.exports = {
   addVoter,
